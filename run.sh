0943db2418355038c5c58a8e2e6ffca67b11685c
Miguel Revilla <yo@miguelrevilla.com>
2020-02-05 16:17:26 +0100

    More logical path names

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-build/bin/ojudgeapp --docroot "Wt;/images,/fonts,/css,/favicon.ico" --resources-dir /usr/share/Wt/resources --http-address 0.0.0.0 --http-port 9090
+build/bin/ojudgeapp --docroot docroot --resources-dir /usr/share/Wt/resources --http-address 0.0.0.0 --http-port 9090
