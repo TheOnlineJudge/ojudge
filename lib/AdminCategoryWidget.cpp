ddd4fc13e342f07d185580ae52864035121da89d
Miguel Revilla <yo@miguelrevilla.com>
2020-02-05 10:57:57 +0100

    Cleaning of old code

--- lib/AdminCategoryWidget.cpp
+++ lib/AdminCategoryWidget.cpp
@@ -28,34 +28,4 @@ AdminCategoryWidget::AdminCategoryWidget(const std::shared_ptr<CategoryModel> ca
 	treeWidget_->setRowHeight(26) ;
 	treeWidget_->setHeaderHeight(26) ;
 	treeWidget_->setSortingEnabled(false) ;
-	
-
-/*	auto result = mainLayout->addWidget(cpp14::make_unique<WTemplate>(WString::tr("lineEdit-template")));
-	result->addFunction("id", &WTemplate::Functions::id);
-
-	auto button = mainLayout->addWidget(cpp14::make_unique<WPushButton>("Create"));
-
-	auto edit = Wt::cpp14::make_unique<Wt::WLineEdit>();
-	
-	result->bindString("label", "Title");
-	result->bindWidget("edit", std::move(edit)) ;*/
-
 }
-
-/*
-void AdminCategoryWidget::populateCategory(int parent, WTreeTableNode *root) {
-
-	Categories categories = dbmodel_->getCategories(parent) ;
-	Dbo::Transaction transaction = dbmodel_->startTransaction();
-
-	for(Categories::const_iterator i = categories.begin(); i != categories.end(); i++) {
-		dbo::ptr<Category> category = *i;
-		auto node = std::make_unique<WTreeTableNode>(std::string(category->title)) ;
-		node->setColumnWidget(1,std::make_unique<WText>("0")) ;
-		node->setColumnWidget(2,std::make_unique<WText>("0")) ;
-		root->addChildNode(std::move(node)) ;
-		
-//		mainLayout_->addWidget(std::move(cpp14::make_unique<AdminCategoryCategoryWidget>(std::string(category->title),(category->children.size() ? true : false)))) ;
-	}
-}
-*/
