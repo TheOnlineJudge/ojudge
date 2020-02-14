ae4979d5f4cba970311d4e05f5c99898da831c7b
Miguel Revilla <yo@miguelrevilla.com>
2020-02-14 11:49:37 +0100

    Added addCategory/addProblem dialogs. Not actually adding the items yet.

--- lib/AdminCategoryWidget.cpp
+++ lib/AdminCategoryWidget.cpp
@@ -14,11 +14,12 @@
 #include <Wt/WHBoxLayout.h>
 #include <Wt/WImage.h>
 #include <Wt/WCssDecorationStyle.h>
+#include <Wt/WTemplate.h>
 #include "AdminCategoryWidget.h"
 
 using namespace Wt;
 
-AdminCategoryWidget::AdminCategoryWidget(const std::shared_ptr<CategoryModel> catmodel) {
+AdminCategoryWidget::AdminCategoryWidget(const std::shared_ptr<CategoryModel> catmodel) : catmodel_(catmodel) {
 
 	mainLayout_ = setLayout(cpp14::make_unique<WVBoxLayout>());
 	mainLayout_->setContentsMargins(0,0,0,0);
@@ -30,13 +31,57 @@ AdminCategoryWidget::AdminCategoryWidget(const std::shared_ptr<CategoryModel> ca
 	addButton->setHeight(WLength(32));
 	addButton->decorationStyle().setCursor(Cursor::PointingHand);
 	addButton->setToolTip(WString("Add new category"));
+	addButton->clicked().connect(this,&AdminCategoryWidget::showAddDialog);
 	toolbarLayout->addStretch(1);
 
 	treeWidget_ = mainLayout_->addWidget(cpp14::make_unique<WTreeView>());
 
-	treeWidget_->setModel(catmodel);
+	treeWidget_->setModel(catmodel_);
 
 	treeWidget_->setRowHeight(26);
 	treeWidget_->setHeaderHeight(26);
 	treeWidget_->setSortingEnabled(false);
+}
+
+void AdminCategoryWidget::showAddDialog() {
+
+	addDialog_ = addChild(cpp14::make_unique<WDialog>("Add new category"));
+
+	auto result = addDialog_->contents()->addWidget(cpp14::make_unique<WTemplate>(WString::tr("addCategory-template")));
+	result->addFunction("id",&WTemplate::Functions::id);
+
+	auto selectParent = cpp14::make_unique<WTreeView>();
+	selectParent->setModel(catmodel_);
+	selectParent->setSortingEnabled(false);
+	selectParent->setColumnHidden(1,true);
+	selectParent->setColumnHidden(2,true);
+	selectParent->setColumnHidden(3,true);
+	selectParent->setSelectionMode(SelectionMode::Single);
+	selectParent_ = selectParent.get();
+	auto rootCheckBox = cpp14::make_unique<WCheckBox>("Root category");
+	rootCheckBox->checked().connect( [=] { selectParent_->setHidden(true); } );
+	rootCheckBox->unChecked().connect( [=] { selectParent_->setHidden(false); } );
+	rootCheckBox_ = rootCheckBox.get();
+	auto newTitle = cpp14::make_unique<WLineEdit>();
+	newTitle_ = newTitle.get();
+	
+
+	result->bindString("treelabel",WString("Select category parent"));
+	result->bindString("titlelabel",WString("Title"));
+	result->bindWidget("rootcheckbox",std::move(rootCheckBox));
+	result->bindWidget("tree",std::move(selectParent));
+	result->bindWidget("title",std::move(newTitle));
+
+	WPushButton *ok = addDialog_->footer()->addWidget(cpp14::make_unique<WPushButton>("Ok"));
+	WPushButton *cancel = addDialog_->footer()->addWidget(cpp14::make_unique<WPushButton>("Cancel"));
+
+	ok->clicked().connect(addDialog_, &WDialog::accept);
+	cancel->clicked().connect(addDialog_, &WDialog::reject);
+
+	addDialog_->finished().connect(this,&AdminCategoryWidget::addDialogDone);
+	addDialog_->show();
+}
+
+void AdminCategoryWidget::addDialogDone(DialogCode code) {
+	removeChild(addDialog_);
 }
--- lib/AdminCategoryWidget.h
+++ lib/AdminCategoryWidget.h
@@ -13,6 +13,9 @@
 #include <Wt/WContainerWidget.h>
 #include <Wt/WVBoxLayout.h>
 #include <Wt/WTreeView.h>
+#include <Wt/WDialog.h>
+#include <Wt/WLineEdit.h>
+#include <Wt/WCheckBox.h>
 #include "viewmodels/CategoryModel.h"
 
 class AdminCategoryWidget : public Wt::WContainerWidget {
@@ -20,8 +23,15 @@ public:
 AdminCategoryWidget(const std::shared_ptr<CategoryModel> catmodel);
 
 private:
+const std::shared_ptr<CategoryModel> catmodel_;
 Wt::WVBoxLayout *mainLayout_;
 Wt::WTreeView *treeWidget_;
+Wt::WDialog *addDialog_;
+Wt::WCheckBox *rootCheckBox_;
+Wt::WTreeView *selectParent_;
+Wt::WLineEdit *newTitle_;
+void showAddDialog();
+void addDialogDone(Wt::DialogCode code);
 };
 
 #endif // ADMINCATEGORYWIDGET_H
