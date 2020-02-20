e10a54d241cde31cbece7ad7237b17ac0d817228
enjolras <yo@miguelrevilla.com>
2020-02-20 20:24:41 +0100

    Admin category (#45)
    * Added up, down and trash icons
    
    * Added edit icon
    
    * More on adding a category

--- lib/AdminCategoryWidget.h
+++ lib/AdminCategoryWidget.h
@@ -16,7 +16,10 @@
 #include <Wt/WDialog.h>
 #include <Wt/WLineEdit.h>
 #include <Wt/WCheckBox.h>
+#include <Wt/WWidget.h>
 #include "viewmodels/CategoryModel.h"
+
+#include <Wt/WAbstractItemDelegate.h>
 
 class AdminCategoryWidget : public Wt::WContainerWidget {
 public:
@@ -26,12 +29,33 @@ private:
 const std::shared_ptr<CategoryModel> catmodel_;
 Wt::WVBoxLayout *mainLayout_;
 Wt::WTreeView *treeWidget_;
-Wt::WDialog *addDialog_;
+Wt::WDialog *addEditDialog_;
 Wt::WCheckBox *rootCheckBox_;
 Wt::WTreeView *selectParent_;
-Wt::WLineEdit *newTitle_;
-void showAddDialog();
-void addDialogDone(Wt::DialogCode code);
+Wt::WLineEdit *title_;
+void showAddEditDialog(const Wt::WModelIndex& index = Wt::WModelIndex());
+void addEditDialogDone(Wt::DialogCode code);
+};
+
+class AdminActionsDelegate : public Wt::WAbstractItemDelegate {
+public:
+AdminActionsDelegate();
+virtual std::unique_ptr<Wt::WWidget> update(Wt::WWidget *widget, const Wt::WModelIndex& index, Wt::WFlags<Wt::ViewItemRenderFlag> flags) override;
+Wt::Signal<const Wt::WModelIndex&>& editCategory() { return editCategory_; }
+Wt::Signal<const Wt::WModelIndex&>& deleteCategory() { return deleteCategory_; }
+Wt::Signal<const Wt::WModelIndex&,bool>& moveCategory() { return moveCategory_; }
+
+private:
+struct WidgetRef {
+	std::unique_ptr<Wt::WWidget> created;
+	Wt::WWidget *w;
+	WidgetRef(Wt::WWidget *widget) : w(widget) { }
+};
+
+Wt::Signal<const Wt::WModelIndex&> editCategory_ ;
+Wt::Signal<const Wt::WModelIndex&> deleteCategory_ ;
+Wt::Signal<const Wt::WModelIndex&,bool> moveCategory_ ;
+
 };
 
 #endif // ADMINCATEGORYWIDGET_H
