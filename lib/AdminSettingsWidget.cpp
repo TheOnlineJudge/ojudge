b3856a7da6656ea6b410bc8092f2bfd10452f7c5
Miguel Revilla <yo@miguelrevilla.com>
2020-02-24 19:29:03 +0100

    Layout changes and some design in admin area

--- lib/AdminSettingsWidget.cpp
+++ lib/AdminSettingsWidget.cpp
@@ -9,6 +9,10 @@
 
 #include <Wt/WLineEdit.h>
 #include <Wt/WTemplate.h>
+#include <Wt/WMenu.h>
+#include <Wt/WStackedWidget.h>
+#include <Wt/WHBoxLayout.h>
+#include <Wt/WContainerWidget.h>
 #include "dbmodel/DBModel.h"
 #include "AdminSettingsWidget.h"
 
@@ -17,12 +21,20 @@ using namespace Wt;
 AdminSettingsWidget::AdminSettingsWidget(DBModel *dbmodel) : dbmodel_(dbmodel) {
 
 	Settings settings = dbmodel_->getSettings();
-
 	Dbo::Transaction transaction = dbmodel_->startTransaction();
+
+	auto mainLayout = setLayout(cpp14::make_unique<WHBoxLayout>());
+
+	auto mainStack = mainLayout->addWidget(cpp14::make_unique<WStackedWidget>(),1);
+	auto leftMenu = mainLayout->insertWidget(0,cpp14::make_unique<WMenu>(mainStack),0);
+	leftMenu->addStyleClass("flex-column");
+	leftMenu->setWidth(200);
+
+	auto generalSettings = cpp14::make_unique<WContainerWidget>();
 
 	for(Settings::const_iterator i = settings.begin(); i != settings.end(); i++) {
 		dbo::ptr<Setting> setting = *i;
-		auto result = addWidget(cpp14::make_unique<WTemplate>(WString::tr("lineEdit-template")));
+		auto result = generalSettings->addWidget(cpp14::make_unique<WTemplate>(WString::tr("lineEdit-template")));
 		result->addFunction("id",&WTemplate::Functions::id);
 
 		auto edit = cpp14::make_unique<WLineEdit>(setting->settingValue);
@@ -30,4 +42,9 @@ AdminSettingsWidget::AdminSettingsWidget(DBModel *dbmodel) : dbmodel_(dbmodel) {
 		result->bindString("label",WString::tr(setting->settingName));
 		result->bindWidget("edit", std::move(edit));
 	}
+
+	auto generalSettingsMenu = leftMenu->addItem("General",std::move(generalSettings));
+	auto mailSettingsMenu = leftMenu->addItem("Mail",cpp14::make_unique<WContainerWidget>());
+	auto contactSettingsMenu = leftMenu->addItem("Contact",cpp14::make_unique<WContainerWidget>());
+	auto footerSettingsMenu = leftMenu->addItem("Footer",cpp14::make_unique<WContainerWidget>());
 }
