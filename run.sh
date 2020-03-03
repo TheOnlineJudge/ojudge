1211ad167b4efb6e3fc9be8ad64a587126c5c499
Miguel Revilla <yo@miguelrevilla.com>
2020-03-03 17:14:51 +0100

    Remove ace from run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-build/bin/ojudgeapp --docroot "docroot;/favicon.ico,/ace,/css,/resources,/images" --resources-dir /usr/share/Wt/resources --http-address 0.0.0.0 --http-port 9090
+build/bin/ojudgeapp --docroot "docroot;/favicon.ico,/css,/resources,/images" --resources-dir /usr/share/Wt/resources --http-address 0.0.0.0 --http-port 9090
