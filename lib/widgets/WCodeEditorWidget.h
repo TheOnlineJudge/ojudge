4a880906800e84172d61b2da236bcad02025a0c1
Miguel Revilla <yo@miguelrevilla.com>
2020-03-03 09:26:31 +0100

    Added WCodeEditorWidget skel

---
+++ lib/widgets/WCodeEditorWidget.h
@@ -0,0 +1,14 @@
+/*********************************************************************
+* Copyright (C) 2020 Miguel Revilla Rodríguez
+*                    and the OJudge Platform project contributors
+*
+* This file is part of the OJudge Platform
+*
+* Read the LICENSE file for information on license terms
+*********************************************************************/
+
+#ifndef WCODEEDITORWIDGET_H
+#define WCODEEDITORWIDGET_H
+
+
+#endif // WCODEEDITORWIDGET_H
