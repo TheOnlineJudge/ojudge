1b73ea483c8796a15644dd7d19f654f0fdf9c22f
Miguel Revilla <yo@miguelrevilla.com>
2020-03-29 14:45:39 +0200

    Login/Logout signals connected to 'top-level' widgets

--- lib/ojudgeApp.h
+++ lib/ojudgeApp.h
@@ -18,10 +18,11 @@
 #include <Wt/WNavigationBar.h>
 #include <Wt/Auth/AuthModel.h>
 #include <Wt/WAbstractItemModel.h>
+#include <Wt/Auth/Login.h>
+#include <Wt/WSignal.h>
 #include <boost/program_options.hpp>
 #include "viewmodels/ViewModels.h"
 #include "LoginWidget.h"
-#include "ProblemWidget.h"
 
 namespace xmlResources {
 // Common
@@ -33,6 +34,18 @@ extern const char *messages_xml1;
 
 class Session;
 class DBModel;
+
+class ProblemWidget;
+class AboutWidget;
+class AdminWidget;
+class ContactWidget;
+class ContributeWidget;
+class FactsWidget;
+class LanguagesWidget;
+class ProfileWidget;
+class SponsorsWidget;
+class TeamWidget;
+class TutorialWidget;
 
 class ojudgeApp : public Wt::WApplication {
 
@@ -46,16 +59,16 @@ Wt::WMenu *mainMenu_;
 Wt::WMenu *mainFloatMenu_;
 Wt::WStackedWidget *mainStack_;
 ProblemWidget *problemWidget_;
-Wt::WContainerWidget *aboutWidget_;
-Wt::WContainerWidget *adminWidget_;
-Wt::WContainerWidget *contactWidget_;
-Wt::WContainerWidget *contributeWidget_;
-Wt::WContainerWidget *factsWidget_;
-Wt::WContainerWidget *languagesWidget_;
-Wt::WContainerWidget *profileWidget_;
-Wt::WContainerWidget *sponsorsWidget_;
-Wt::WContainerWidget *teamWidget_;
-Wt::WContainerWidget *tutorialWidget_;
+AboutWidget *aboutWidget_;
+AdminWidget *adminWidget_;
+ContactWidget *contactWidget_;
+ContributeWidget *contributeWidget_;
+FactsWidget *factsWidget_;
+LanguagesWidget *languagesWidget_;
+ProfileWidget *profileWidget_;
+SponsorsWidget *sponsorsWidget_;
+TeamWidget *teamWidget_;
+TutorialWidget *tutorialWidget_;
 Wt::WMenuItem *dashboardMenu_;
 Wt::WMenuItem *dashboardFloatMenu_;
 Wt::WMenuItem *loginMenu_;
@@ -65,6 +78,16 @@ Wt::WMenuItem *profileFloatMenu_;
 Wt::WNavigationBar *floatNavBar_;
 Wt::Auth::AuthModel *authModel_;
 std::unique_ptr<LoginWidget> loginWidget_;
+
+Wt::Signal<Wt::Auth::Login&>& loginSignal() {
+	return loginSignal_;
+}
+Wt::Signal<>& logoutSignal() {
+	return logoutSignal_;
+}
+
+Wt::Signal<Wt::Auth::Login&> loginSignal_;
+Wt::Signal<> logoutSignal_;
 
 void pathChanged(std::string newPath);
 void switchNavbar(bool visible);
