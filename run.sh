5a2f9cf1e215c7a8780af1e6179c146b852c5719
Miguel Revilla <yo@miguelrevilla.com>
2020-04-19 10:08:35 +0200

    Bind only to localhost

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-build/bin/ojudgeapp --docroot "docroot;/favicon.ico,/css,/resources,/images" --resources-dir /usr/share/Wt/resources --http-address 0.0.0.0 --http-port 9090
+build/bin/ojudgeapp --docroot "docroot;/favicon.ico,/css,/resources,/images" --resources-dir /usr/share/Wt/resources --http-address 127.0.0.1 --http-port 9090
